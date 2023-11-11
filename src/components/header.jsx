eee073def9b8bb7098df645934e8674a8970e1c1
Sam <samilalghiffary@gmail.com>
2023-11-11 19:30:24 +0700

    refactor: header, layout, navbar, and sidebar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-
-function Header(props) {
-  const {titleHeader} = props;
+function Header({ titleHeader }) {
   return (
-    <div className="p-7">
-      <div className="flex flex-col">
-        <div className=" flex flex-row w-full h-20 bg-white rounded-md drop-shadow-xl justify-between">
-          <h3 className="font-semibold text-2xl p-6">{titleHeader}</h3>
-          <h3 className="font-light text-base p-8">Accounts/Edit User</h3>
-        </div>
+    <div className="flex flex-col">
+      <div className=" flex w-full h-20 bg-white drop-shadow-md justify-between">
+        <h3 className="font-semibold text-2xl p-6">{titleHeader}</h3>
+        <h3 className="font-light text-base p-8">Accounts/Edit User</h3>
       </div>
     </div>
   );
