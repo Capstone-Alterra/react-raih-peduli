367ae1c86658c011b2caa53d794185c1db6376ea
Sam <samilalghiffary@gmail.com>
2023-11-15 22:23:28 +0700

    refactor: Change text color and add icon into action button

--- src/pages/fundraising/index.jsx
+++ src/pages/fundraising/index.jsx
@@ -7,6 +7,7 @@ import Layout from '@/components/layout';
 import DataTable from './fundraising-table';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import CsvIcon from '@/assets/icons/csv';
 
 function Fundraising() {
   const [filtering, setFiltering] = useState('');
@@ -16,12 +17,15 @@ function Fundraising() {
       <Header titleHeader="Penggalangan Dana" />
       <div className="rounded shadow mt-5">
         <div className="flex px-8 gap-3 items-center py-3 border-b-[1px]">
-          <h3 className="text-lg font-bold text-primary">List Penggalangan Dana</h3>
-          <div className="h-[2px] w-5 bg-primary rounded"></div>
-          <Button className="rounded-full" asChild>
+          <h3 className="text-lg font-bold text-[#293066]">List Penggalangan Dana</h3>
+          <div className="h-[2px] w-5 bg-[#293066] rounded"></div>
+          <Button className="rounded-full bg-[#293066] hover:bg-[#293066]/80" asChild>
             <Link to="/test">Tambah Penggalangan Dana</Link>
           </Button>
-          <Button className="rounded-full">Export CSV</Button>
+          <Button className="rounded-full bg-[#14513B] hover:bg-[#14513B]/80 flex gap-1">
+            <CsvIcon className="w-5 h-5" />
+            Export CSV
+          </Button>
         </div>
         <div className="px-8 flex items-center gap-2 py-6">
           Cari :{' '}
