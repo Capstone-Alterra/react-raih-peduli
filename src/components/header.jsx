c62cb77c0fe77ba8cb1933b1b2030176d87cbefc
Sam <samilalghiffary@gmail.com>
2023-11-17 01:42:33 +0700

    feat: Implement breadcrumbs component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
+import Breadcrumbs from './breadcrumbs';
+
 function Header({ titleHeader }) {
   return (
     <div className="flex h-16 bg-white drop-shadow-md justify-between items-center px-8">
       <h3 className="font-bold text-xl text-black">{titleHeader}</h3>
-      <h3 className="font-light text-base p-8">Accounts/Edit User</h3>
+      <Breadcrumbs />
     </div>
   );
 }
