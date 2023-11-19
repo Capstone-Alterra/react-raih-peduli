ccca210103b9e4506adaaf3ca6eae3185918bb52
NajwanNurIman <Najwannur01@gmail.com>
2023-11-19 22:13:45 +0800

    chore: routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,45 +1,44 @@
-import Dashboard from "@/pages/dashboard";
-import FundraisingForm from "@/pages/fundraising/form";
-import Fundraising from "@/pages/fundraising/index";
-import EditUser from "@/pages/customer/edit.jsx";
-import Transaction from "@/pages/transaction";
-import TransactionDetail from "@/pages/transaction/transaction-detail";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import User from "@/pages/customer/index";
+import Dashboard from '@/pages/dashboard';
+import FundraisingForm from '@/pages/fundraising/form';
+import Fundraising from '@/pages/fundraising/index';
+import EditUser from '@/pages/customer/edit.jsx';
+import Transaction from '@/pages/transaction';
+import TransactionDetail from '@/pages/transaction/transaction-detail';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import User from '@/pages/customer/index';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
-      path: "/user",
-      element: <User />,
-    },
-    {
-      path: "/user/edit/:id",
-      element: <EditUser />,
-    },
-
-    {
-      path: "/dashboard",
+      path: '/dashboard',
       element: <Dashboard />,
     },
     {
-      path: "/transaction",
+      path: '/pelanggan',
+      element: <User />,
+    },
+    {
+      path: '/pelanggan/:id',
+      element: <EditUser />,
+    },
+    {
+      path: '/transaksi',
       element: <Transaction />,
     },
     {
-      path: "/transaction/:id",
+      path: '/transaksi/:id',
       element: <TransactionDetail />,
     },
     {
-      path: "/fundraising",
+      path: '/penggalangan-dana',
       element: <Fundraising />,
     },
     {
-      path: "/fundraising/form",
+      path: '/penggalangan-dana/:id',
       element: <FundraisingForm />,
     },
     {
-      path: "*",
+      path: '*',
       element: <div>404 page found</div>,
     },
   ]);
