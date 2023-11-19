b7efe4944d7ca280bbb94318f25fc1c28554d8d5
NajwanNurIman <Najwannur01@gmail.com>
2023-11-19 22:58:15 +0800

    fix: slicing dashboard

--- src/pages/dashboard/table_header.jsx
+++ src/pages/dashboard/table_header.jsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 
 export default function TableHeader() {
   return (
-    <div className="p-5 w-full flex justify-start items-center gap-14 border  ">
+    <div className="p-5 w-full flex justify-start items-center gap-14 border rounded-t-xl">
       <div className="w-188 text-black text-lg font-bold font-nunito break-words">
         List Penggalangan Dana
       </div>
