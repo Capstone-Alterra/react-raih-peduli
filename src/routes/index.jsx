597b24f5869ad88ce44fef19b049dd05d64ee72d
Gilang Setyawan <101815893+setyawan1234@users.noreply.github.com>
2023-11-19 19:10:16 +0700

    refactor : change routing configuration

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,64 @@
-import Dashboard from "@/pages/dashboard";
 import Login from "@/pages/login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ForgotPasswordPage from "@/pages/login/forgot-password/ForgotPasswordPage";
+import OTPPage from "@/pages/login/forgot-password/OTPPage";
+import RePasswordPage from "@/pages/login/forgot-password/RePasswordPage";
+import Dashboard from '@/pages/dashboard';
+import FundraisingForm from '@/pages/fundraising/form';
+import Fundraising from '@/pages/fundraising/index';
+import EditUser from '@/pages/customer/edit.jsx';
+import Transaction from '@/pages/transaction';
+import TransactionDetail from '@/pages/transaction/transaction-detail';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import User from '@/pages/customer/index';
+
 export default function Router() {
   const router = createBrowserRouter([
-    {
-      path: "/dashboard",
-      element: <Dashboard />,
-    },
-    {
-      path: "/fundraising",
-      element: <fundraising />,
-    },
     {
       path: "/login",
       element: <Login />,
     },
     {
-      path: "*",
+      path: "/lupa-password",
+      element: <ForgotPasswordPage />,
+    },
+    {
+      path:"/otp-password",
+      element: <OTPPage />,
+    },
+    {
+      path: "/repassword",
+      element: <RePasswordPage/>
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard />,
+    },
+    {
+      path: '/pelanggan',
+      element: <User />,
+    },
+    {
+      path: '/pelanggan/:id',
+      element: <EditUser />,
+    },
+    {
+      path: '/transaksi',
+      element: <Transaction />,
+    },
+    {
+      path: '/transaksi/:id',
+      element: <TransactionDetail />,
+    },
+    {
+      path: '/penggalangan-dana',
+      element: <Fundraising />,
+    },
+    {
+      path: '/penggalangan-dana/:id',
+      element: <FundraisingForm />,
+    },
+    {
+      path: '*',
       element: <div>404 page found</div>,
     },
   ]);
