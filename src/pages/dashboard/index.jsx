2c3abcaa6d9933ff09ac101f9840619e7bc83d01
Sam <samilalghiffary@gmail.com>
2023-11-20 09:26:03 +0700

    refactor: Change structure compoent

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,34 +1,28 @@
-import React from "react";
+import { columns } from "./columns";
+import Table from "./table-dashboard";
 import Header from "@/components/header";
 import Layout from "@/components/layout";
-import Cards from "@/components/card";
-import Dollar from "@/assets/dashborad/coin.svg"
-import User from "@/assets/dashborad/User.svg"
-import News from "@/assets/dashborad/News.svg"
-import Volunteer from "@/assets/dashborad/hand.svg"
-import { columns } from "./colums";
-import data from "@/pages/dashboard/MOCK_DATA.json"
-import Table from "./table-dashboard";
-import TableHeader from "./table_header";
+import User from "@/assets/dashborad/User.svg";
+import News from "@/assets/dashborad/News.svg";
+import Dollar from "@/assets/dashborad/coin.svg";
+import Volunteer from "@/assets/dashborad/hand.svg";
+import data from "@/pages/dashboard/MOCK_DATA.json";
+import Cards from "@/pages/dashboard/components/card";
 
 function Dashboard() {
   return (
     <>
       <Layout>
-      <Header titleHeader="Dashboard"/>
-        <div className=" flex gap-16 pb-11">
+        <Header titleHeader="Dashboard" />
+        <div className="w-full flex gap-4 mt-6">
           <Cards image={User} title="Pelanggan" customerCount={1} />
-          <Cards image={Dollar} title="Penggalangan Dana" customerCount={1}/>
-          <Cards image={Volunteer} title="Lowongan Sukarelawan" customerCount={1}/>
-          <Cards image={News} title="Berita" customerCount={1}/>
+          <Cards image={Dollar} title="Penggalangan Dana" customerCount={1} />
+          <Cards image={Volunteer} title="Lowongan Sukarelawan" customerCount={50} />
+          <Cards image={News} title="Berita" customerCount={1} />
         </div>
-        <div className="flex gap-10  ">
-          <TableHeader/>
-          <TableHeader/>
-        </div>
-        <div className="flex gap-10 pb-10">
-        <Table columns={columns} data={data} />
-        <Table columns={columns} data={data}/>
+        <div className="flex gap-4 mt-6">
+          <Table columns={columns} data={data} />
+          <Table columns={columns} data={data} />
         </div>
       </Layout>
     </>
--- src/pages/dashboard/table-dashboard.jsx
+++ src/pages/dashboard/table-dashboard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import {
   Table,
   TableBody,
@@ -8,55 +6,65 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import {
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 
-export default function TableDashboard({columns,data}) {
+function TableDashboard({ columns, data }) {
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
+
   return (
-    <Table className= "border border-b">
-      <TableHeader className="bg-[#F5F5F5]">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <TableHead className="whitespace-nowrap" key={header.id}>
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </TableHead>
-              );
-            })}
-          </TableRow>
-        ))}
-      </TableHeader>
-      <TableBody>
-        {table.getRowModel().rows?.length ? (
-          table.getRowModel().rows.map((row) => (
-            <TableRow key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
+    <div className="w-full rounded overflow-auto shadow border-[1px] border-[#D1D1D1] flex flex-col mb-6">
+      <div className="h-16 px-4 flex justify-between items-center">
+        <div className="text-black text-md font-bold font-nunito break-words">
+          List Penggalangan Dana
+        </div>
+        <Button size="sm" className="bg-[#293066] hover:bg-[#293066]/80 rounded-full">
+          Lihat semua
+        </Button>
+      </div>
+      <Table className="w-full">
+        <TableHeader className="bg-[#F5F5F5]">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead className="whitespace-nowrap" key={header.id}>
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
             </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={columns.length} className="h-24 text-center">
-              Data tidak tersedia.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    style={{ maxHeight: "100px", overflow: "hidden", textOverflow: "ellipsis" }}
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                Data tidak tersedia.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
+
+export default TableDashboard;
