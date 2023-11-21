085c08047d704b2e902a4e5da0ed39f772d83f7f
Sam <samilalghiffary@gmail.com>
2023-11-21 22:39:38 +0700

    feat: Implement axios inteceptor and add env for base url

--- src/utils/api/axiosWithConfig.js
+++ src/utils/api/axiosWithConfig.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
+let accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDA1ODAwNDMsImlhdCI6MTcwMDU3OTQ0Mywicm9sZV9pZCI6IjIiLCJ1c2VyX2lkIjoiMzMifQ.Wvc3eIjCTx_QUmAD_r4MwKClqa21Yp5iz3cFK5llzxE";
+
 const axiosWithConfig = axios.create({
-  baseURL: "http://35.197.137.181:8000",
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
+
+// export const setAxiosConfig = (token) => {
+//   accessToken = token;
+// };
+
+axiosWithConfig.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
 });
 
 export default axiosWithConfig;
