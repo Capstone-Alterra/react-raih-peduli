d498e8236a07ca6a3cc41b44269cd78d6d5db91f
Sam <samilalghiffary@gmail.com>
2023-11-21 22:37:12 +0700

    refactor: Change link href and improved fetching data

--- src/pages/fundraising/index.jsx
+++ src/pages/fundraising/index.jsx
@@ -1,29 +1,22 @@
-import { columns } from "./fundraise-columns";
 import { Link } from "react-router-dom";
 import CsvIcon from "@/assets/icons/csv";
 import Header from "@/components/header";
 import Layout from "@/components/layout";
 import { useEffect, useState } from "react";
+import { columns } from "./fundraise-columns";
 import { Button } from "@/components/ui/button";
+import { getFundraises } from "@/utils/api/fundraise";
 import TableData from "@/components/table/table-data";
 import TableHeader from "@/components/table/table-header";
 import TableLayout from "@/components/table/table-layout";
-import axiosWithConfig from "@/utils/api/axiosWithConfig";
 
 function Fundraise() {
   const [fundraises, setFundraises] = useState([]);
 
-  const getFundraises = async () => {
-    try {
-      const response = await axiosWithConfig.get("/fundraises");
-      setFundraises(response.data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
-    getFundraises();
+    getFundraises()
+      .then((data) => setFundraises(data))
+      .catch((message) => alert(message));
   }, []);
 
   return (
@@ -32,7 +25,7 @@ function Fundraise() {
       <TableLayout>
         <TableHeader heading="List Penggalangan Dana" hasAction={true}>
           <Button size="sm" className="rounded-full bg-[#293066] hover:bg-[#293066]/80" asChild>
-            <Link to="/tambah-penggalangan-dana">Tambah Penggalangan Dana</Link>
+            <Link to="/penggalangan-dana/tambah-penggalangan-dana">Tambah Penggalangan Dana</Link>
           </Button>
           <Button size="sm" className="rounded-full bg-[#14513B] hover:bg-[#14513B]/80 flex gap-1">
             <CsvIcon className="w-5 h-5" />
