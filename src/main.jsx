221f720985f784fcd872df83578a38b667bc3c70
Gilang Setyawan <101815893+setyawan1234@users.noreply.github.com>
2023-11-25 19:28:45 +0700

    refactor: wrapper app inside a token-provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/routes/index'
 import '@/styles/index.css'
-
+import { TokenProvider } from './utils/context/token'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TokenProvider>
+      <App />
+    </TokenProvider>
   </React.StrictMode>,
 )
