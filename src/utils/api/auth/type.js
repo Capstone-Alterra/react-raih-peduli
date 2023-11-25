53df36c74505ed006511b96919395eb0d88bffea
Gilang Setyawan <101815893+setyawan1234@users.noreply.github.com>
2023-11-25 19:32:03 +0700

    .

--- src/utils/api/auth/type.js
+++ src/utils/api/auth/type.js
@@ -4,6 +4,3 @@ export const loginSchema = z.object({
     email: z.string().min(1, { message: "Email tidak boleh kosong" }).email({ message: "Penggunaan email tidak valid" }),
     password: z.string().min(1, { message: "Password tidak boleh kosong" })
 })
-
-email: z
-    
