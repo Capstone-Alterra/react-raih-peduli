7c7b2eff569d3511d21bd7b17c0fbce3b84619da
Sam <samilalghiffary@gmail.com>
2023-11-25 07:24:08 +0700

    refactor: uncomment func setAxiosConfig

--- src/utils/api/axiosWithConfig.js
+++ src/utils/api/axiosWithConfig.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 let accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDA1ODAwNDMsImlhdCI6MTcwMDU3OTQ0Mywicm9sZV9pZCI6IjIiLCJ1c2VyX2lkIjoiMzMifQ.Wvc3eIjCTx_QUmAD_r4MwKClqa21Yp5iz3cFK5llzxE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlX2lkIjoiMiIsInVzZXJfaWQiOiIxIiwiaWF0IjoxNTE2MjM5MDIyfQ.rdcs2MjPJKRFRLQvBIqttWf3KCBD5vkWe73_w3OJ5PM";
 
 const axiosWithConfig = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-// export const setAxiosConfig = (token) => {
-//   accessToken = token;
-// };
+export const setAxiosConfig = (token) => {
+  accessToken = token;
+};
 
 axiosWithConfig.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${accessToken}`;
