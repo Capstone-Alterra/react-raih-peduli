3aca41cfb91ef157797a8aa67a60c9d75bc0943b
Gilang Setyawan <101815893+setyawan1234@users.noreply.github.com>
2023-11-25 19:33:09 +0700

    refactor: recode token.jsx and add a cookie for token

--- src/utils/context/token.jsx
+++ src/utils/context/token.jsx
@@ -1,58 +1,70 @@
 import {
-    createContext,
-    useState,
-    useMemo,
-    useContext,
-    useCallback,
-  } from "react";
-  
-  const contextValue = {
-    token: "",
-    changeToken: () => {},
-  };
-  
-  const TokenContext = createContext(contextValue);
-  
-  function TokenProvider({ children }) {
-    const initialValue = localStorage.getItem("admin") ?? "";
-    const [token, setToken] = useState(initialValue);
-  
-    const changeToken = useCallback(
-      (data) => {
-        const newData = data ?? "";
-        if (data) {
-          localStorage.setItem("admin", newData);
-        }else {
-            localStorage.removeItem("admin");
-        }
-        setToken(newData);
-      },
-      [token]
-    );
-  
-    const tokenContextValue = useMemo(
-      () => ({
-        token,
-        changeToken,
-      }),
-      [token]
-    );
-  
-    return (
-      <TokenContext.Provider value={tokenContextValue}>
-        {children}
-      </TokenContext.Provider>
-    );
-  }
-  
-  function useToken() {
-    const tokenContext = useContext(TokenContext);
-  
-    if (tokenContext === undefined) {
-      console.log("ERROR, useToken must be used within TokenContext");
+  createContext,
+  useState,
+  useMemo,
+  useContext,
+  useCallback,
+} from "react";
+
+import { useCookies } from "react-cookie";
+import axiosWithConfig from "../setAxiosWithConfig";
+
+const contextValue = {
+  token: "",
+  changeToken: () => {},
+};
+
+const TokenContext = createContext(contextValue);
+
+function TokenProvider({ children }) {
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  const [token, setToken] = useState(cookies.token);
+
+  axiosWithConfig.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401) {
+        changeToken("");
+      }
+      return Promise.reject(error);
     }
-  
-    return tokenContext;
+  );
+
+  const changeToken = useCallback((token) => {
+    const newToken = token ?? "";
+    setToken(newToken);
+    if (token) {
+      setCookie("token", newToken, { path: "/" });
+    } else {
+      removeCookie("token", { path: "/" });
+    }
+  });
+
+  const tokenContextValue = useMemo(
+    () => ({
+      token,
+      changeToken,
+    }),
+    [token]
+  );
+
+  return (
+    <TokenContext.Provider value={tokenContextValue}>
+      {children}
+    </TokenContext.Provider>
+  );
+}
+
+function useToken() {
+  const tokenContext = useContext(TokenContext);
+
+  if (tokenContext === undefined) {
+    console.log("ERROR, useToken must be used within TokenContext");
   }
-  
-  export { TokenProvider, useToken };
+
+  return tokenContext;
+}
+
+export { TokenProvider, useToken };
