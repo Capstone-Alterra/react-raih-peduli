dd8796a809cfb8787b198272b4a6fedfdcb6d362
Gilang Setyawan <101815893+setyawan1234@users.noreply.github.com>
2023-11-27 19:55:13 +0700

    Integrasi login (#38)
    * code: integrasi login
    
    * refactor: separate auth api
    
    * refactor: wrapper app inside a token-provider
    
    * refactor: recode func login and get access_token
    
    * refactor: recode navbar component and add func logout
    
    * refactor: add protect route
    
    * .
    
    * refactor: recode token.jsx and add a cookie for token
    
    * .
    
    * Fix: protected route & Token
    
    * fix: integrasi login
    
    * fix: resolve review
    
    * fix: bug token
    
    * fix: delete console.log in route
    
    * feat: refresh token every 10 min
    
    ---------
    
    Co-authored-by: Johansyah Diaz <johansyah.diaz8@gmail.com>
    Co-authored-by: Johansyah Diaz <43780278+johansyahdiaz@users.noreply.github.com>

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,23 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import dropdown from "@/assets/logos/DropDown.png";
 import hamburgerIcon from "@/assets/logos/hamburger.svg";
 import useStore from "@/utils/store/store";
+import { useToken } from "@/utils/context/token";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const { token, changeToken } = useToken();
   const { toggleSidebar } = useStore();
+
+  function handleLogout() {
+    changeToken();
+    navigate("/login");
+  }
 
   return (
     <div className="sticky top-0 z-10 h-16 bg-[#293066] flex items-center justify-between shadow-md px-8">
-      <img
-        id="toggling-sidebar"
-        className="w-7 h-7 cursor-pointer"
-        src={hamburgerIcon}
-        onClick={toggleSidebar}
-      />
+      <img className="w-7 h-7 cursor-pointer" src={hamburgerIcon} onClick={toggleSidebar} />
       <DropdownMenu>
         <DropdownMenuTrigger
           id="toggling-profile-dropdown"
@@ -29,13 +26,18 @@ function Navbar() {
           Hi, Admin!
           <img src={dropdown} />
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
+
+        <DropdownMenuContent className={token === "" ? "hidden" : ""}>
+          <div className="px-3 py-2 text-[#F64C4C] cursor-pointer flex gap-2">
+            <img id="btn-logout" src="./btn-logout.svg" alt="btn-logout" className="w-5 h-5" />
+            {token === "" ? (
+              <></>
+            ) : (
+              <p className="mt-[-0.125rem]" onClick={() => handleLogout()}>
+                Keluar
+              </p>
+            )}
+          </div>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
