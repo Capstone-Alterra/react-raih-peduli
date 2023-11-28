998e306c06f5b4099fb5e1fa2218f1264e583bfc
irwanadinata <irwanadinata9@gmail.com>
2023-11-29 03:48:08 +0700

    Refactor: news-columns.jsx

--- src/pages/news/components/news-columns.jsx
+++ src/pages/news/components/news-columns.jsx
@@ -3,11 +3,12 @@ import InfoIcon from '@/assets/icons/info';
 import TrashIcon from '@/assets/icons/trash';
 import PencilIcon from '@/assets/icons/pencil';
 import { Button } from '@/components/ui/button';
+import { deleteNews } from "@/utils/api/news";
 
 export const columns = [
   {
-    header: 'No',
-    accessorKey: 'no',
+    header: "No",
+    accessorFn: (originalRow, index) => index + 1,
   },
   {
     header: 'Judul',
@@ -15,7 +16,7 @@ export const columns = [
   },
   {
     header: 'Gambar',
-    accessorKey: 'image',
+    accessorKey: 'photo',
   },
   {
     header: 'Deskripsi',
@@ -25,7 +26,7 @@ export const columns = [
   {
     header: 'Aksi',
     cell: ({ row }) => {
-      const id = row.original.no;
+      const id = row.original.id;
 
       return (
         <div className="flex gap-2">
@@ -35,7 +36,7 @@ export const columns = [
             className="bg-[#E28100] hover:bg-[#E28100]/80"
             style={{ boxShadow: '0px 4px 4px 0px #00000040' }}
           >
-            <Link to={`/penggalangan-dana/${id}`}>
+            <Link to={`/berita/${id}?edit=true`}>
               <PencilIcon className="w-4 h-4" />
             </Link>
           </Button>
@@ -45,7 +46,7 @@ export const columns = [
             className="bg-[#166648] hover:bg-[#166648]/80"
             style={{ boxShadow: '0px 4px 4px 0px #00000040' }}
           >
-            <Link to={`/penggalangan-dana/${id}`}>
+            <Link to={`/berita/${id}`}>
               <InfoIcon className="w-4 h-4" />
             </Link>
           </Button>
@@ -53,7 +54,15 @@ export const columns = [
             size="icon"
             className="bg-[#BF1616] hover:bg-[#BF1616]/80"
             style={{ boxShadow: '0px 4px 4px 0px #00000040' }}
-            onClick={() => alert(`Hapus penggalangan dana dengan id: ${id}`)}
+            onClick={() => {
+              if (confirm("Hapus Berita?")) {
+                deleteNews(id)
+                  .then((message) => alert(message))
+                  .catch((message) => alert(message));
+              } else {
+                alert("Batal hapus Berita");
+              }
+            }}
           >
             <TrashIcon className="w-4 h-4" />
           </Button>
