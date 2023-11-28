754c9e5a4f227964f8ac14657c385a70eb558a0b
Sam <samilalghiffary@gmail.com>
2023-11-28 17:15:56 +0700

    fix: Resolve issue incorrect date validation error

--- src/utils/api/fundraise/schema.js
+++ src/utils/api/fundraise/schema.js
@@ -7,12 +7,16 @@ export const fundraiseSchema = z.object({
   description: z.string().min(2, {
     message: "Kolom deskripsi penggalangan dana harus diisi",
   }),
-  target: z.any(),
+  target: z.number({ invalid_type_error: "Kolom target penggalangan dana harus diisi" }).min(2, {
+    message: "Kolom target penggalangan dana harus diisi",
+  }),
   start_date: z.date({
     required_error: "Masukkan tanggal mulai penggalangan dana",
+    invalid_type_error: "Masukkan tanggal berakhir penggalangan dana",
   }),
   end_date: z.date({
     required_error: "Masukkan tanggal berakhir penggalangan dana",
+    invalid_type_error: "Masukkan tanggal berakhir penggalangan dana",
   }),
   photo: z.any(),
 
