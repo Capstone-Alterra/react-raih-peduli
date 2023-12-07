c9ec29fb13efcdd0ba45d0fc75969f70ed4a38a6
Sam <samilalghiffary@gmail.com>
2023-12-07 09:40:44 +0700

    refactor: Remove unused SearchParams hook and update props for Header and Form components

--- src/pages/transaction/detail-transaction.jsx
+++ src/pages/transaction/detail-transaction.jsx
@@ -1,22 +1,19 @@
 import Header from "@/components/header";
 import Layout from "@/components/layout";
+import { useParams } from "react-router-dom";
+import TransactionForm from "./components/form";
 import FormLayout from "./components/form-layout";
 import FormHeader from "./components/form-header";
-import { useParams, useSearchParams } from "react-router-dom";
-import TransactionForm from "./components/form";
 
 function DetailTransaction() {
   const { id } = useParams();
-  const [searchParams] = useSearchParams();
-
-  let action = "detail";
 
   return (
     <Layout>
       <Header titleHeader="Transaksi" />
       <FormLayout>
-        <FormHeader id={id} title={action} />
-        <TransactionForm action={action} id={id} />
+        <FormHeader title="detail" />
+        <TransactionForm action="detail" id={id} />
       </FormLayout>
     </Layout>
   );
