94a7109b8654cb188839618f3c581a35d042b806
NajwanNurIman <Najwannur01@gmail.com>
2023-12-08 23:40:37 +0800

    dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,92 +1,123 @@
-import { columns } from "./columns";
 import { useEffect, useState } from "react";
 import { useDebounce } from "@uidotdev/usehooks";
-import { getFundraises,getTotalDataFundraise } from "@/utils/api/fundraise/api";
-import { getTotalDataNews} from "@/utils/api/news/api";
+import { getFundraises, getTotalDataFundraise } from "@/utils/api/fundraise/api";
+import { getTotalDataNews } from "@/utils/api/news/api";
 import { getTotalDataCustomer } from "@/utils/api/customer/api";
+import { getTotalDataVolunteer, getVolunteer } from "@/utils/api/volunter/api";
 import Table from "./table-dashboard";
 import Header from "@/components/header";
 import Layout from "@/components/layout";
 import User from "@/assets/dashborad/User.svg";
 import News from "@/assets/dashborad/News.svg";
 import Dollar from "@/assets/dashborad/coin.svg";
-import Volunteer from "@/assets/dashborad/hand.svg";
+import Volunteer from "@/assets/dashborad/Hand.svg";
 import Cards from "@/pages/dashboard/components/card";
-import datas from "@/pages/dashboard/MOCK_DATA.json"
+import { columns } from "./columns";
+
 function Dashboard() {
-  const [data, setData] = useState([]);
-  const [pageCount, setPageCount] = useState();
+  const [fundraiseData, setFundraiseData] = useState([]); 
+  const [volunteerData, setVolunteerData] = useState([]); 
+  const [fundraisePageCount, setFundraisePageCount] = useState();
+  const [volunteerPageCount, setVolunteerPageCount] = useState();
   const [filtering, setFiltering] = useState("");
-  const debouncedSearchTerm = useDebounce(filtering, 500);
+  const debouncedSearchTerm = useDebounce(filtering, 100);
   const [{ pageIndex, pageSize }, setPagination] = useState({
     pageIndex: 1,
     pageSize: 5,
   });
 
-
-  const [totalDataFundaraise, setTotalDataFundraise] = useState(0);
+  const [totalDataFundraise, setTotalDataFundraise] = useState(0);
   const [totalDataNews, setTotalDataNews] = useState(0);
-  const [totalData, setTotalData] = useState(0);
+  const [totalDataCustomer, setTotalDataCustomer] = useState(0);
+  const [totalDataVolunteer, setTotalDataVolunteer] = useState(0);
 
+  // Mengambil banyaknya data pada Data Fundraise
   useEffect(() => {
     const fetchData = async () => {
       const total = await getTotalDataFundraise();
       setTotalDataFundraise(total);
     };
-
     fetchData();
   }, []);
 
+  // Mengambil banyaknya data pada Data Berita/News
   useEffect(() => {
     const fetchData = async () => {
       const total = await getTotalDataNews();
       setTotalDataNews(total);
     };
-
     fetchData();
   }, []);
 
+  // Mengambil banyaknya data pada Data Customer/User
   useEffect(() => {
-    async function fetchData() {
+    const fetchData = async () => {
       try {
         const total = await getTotalDataCustomer();
-        setTotalData(total);
+        setTotalDataCustomer(total);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching customer data:', error);
       }
-    }
+    };
+    fetchData();
+  }, []);
 
+  // Mengambil banyaknya data pada Data Volunteer
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const total = await getTotalDataVolunteer();
+        setTotalDataVolunteer(total);
+      } catch (error) {
+        console.error('Error fetching volunteer data:', error);
+      }
+    };
     fetchData();
   }, []);
 
   useEffect(() => {
     getFundraises(pageIndex, pageSize, debouncedSearchTerm)
       .then((data) => {
-        setData(data.data);
-        setPageCount(data.pagination.total_page);
+        setFundraiseData(data.data);
+        setFundraisePageCount(data.pagination.total_page);
         setPagination((prevState) => ({
           ...prevState,
           pageIndex: data.pagination.current_page,
         }));
       })
       .catch(() => {
-        setData([]);
+        setFundraiseData([]);
       });
   }, [pageIndex, pageSize, debouncedSearchTerm]);
+
+  // useEffect(() => {
+  //   getVolunteer(pageIndex, pageSize, debouncedSearchTerm)
+  //     .then((data) => {
+  //       setVolunteerData(data.data);
+  //       setVolunteerPageCount(data.pagination.total_page);
+  //       setPagination((prevState) => ({
+  //         ...prevState,
+  //         pageIndex: data.pagination.current_page,
+  //       }));
+  //     })
+  //     .catch(() => {
+  //       setVolunteerData([]);
+  //     });
+  // }, [pageIndex, pageSize, debouncedSearchTerm]);
 
   return (
     <>
       <Layout>
         <Header titleHeader="Dashboard" />
         <div className="w-full flex gap-4 mt-6">
-          <Cards image={User} title="Pelanggan" count={totalData} />
-          <Cards image={Dollar} title="Penggalangan Dana" count={totalDataFundaraise} />
-          <Cards image={Volunteer} title="Lowongan Sukarelawan" count={1} />
+          <Cards image={User} title="Pelanggan" count={totalDataCustomer} />
+          <Cards image={Dollar} title="Penggalangan Dana" count={totalDataFundraise} />
+          <Cards image={Volunteer} title="Lowongan Sukarelawan" count={totalDataVolunteer} />
           <Cards image={News} title="Berita" count={totalDataNews} />
         </div>
         <div className="flex gap-4 mt-6">
-          <Table header={'List Penggalangan Dana'} button={'/penggalangan-dana'} columns={columns} data={data} />
-          <Table header={'List Daftar Relawan'} columns={columns} data={datas} />
+          <Table header={'List Penggalangan Dana'} button={'/penggalangan-dana'} columns={columns} data={fundraiseData} />
+          <Table header={'List Daftar Relawan'} columns={columns} data={volunteerData} />
         </div>
       </Layout>
     </>
--- src/utils/api/volunter/api.js
+++ src/utils/api/volunter/api.js
@@ -1,32 +1,32 @@
-// import axiosWithConfig from "../axiosWithConfig";
+import axiosWithConfig from "../axiosWithConfig";
 
-// export const getVolunter = async (pageIndex, pageSize, title) => {
-//   try {
-//     if (title) {
-//       const response = await axiosWithConfig.get(`/volunteer-vacancies?&title=${title}`);
-//       return response.data;
-//     } else {
-//       const response = await axiosWithConfig.get(
-//         `/volunteer-vacancies?page=${pageIndex}&page_size=${pageSize}&title=${title}`
-//       );
-//       return response.data;
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     throw error;
-//   }
-// };
+export const getVolunteer = async (pageIndex, pageSize, title) => {
+  try {
+    if (title) {
+      const response = await axiosWithConfig.get(`/volunteer-vacancies?&title=${title}`);
+      return response.data;
+    } else {
+      const response = await axiosWithConfig.get(
+        `/volunteer-vacancies?page=${pageIndex}&page_size=${pageSize}&title=${title}`
+      );
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 
-// export const getTotalDataVolunteer = async () => {
-//     try {
-//       const response = await axiosWithConfig.get('/volunteer-vacancies');
-//       const { total_data } = response.data.pagination;
-//       return total_data;
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//       return 0;
-//     }
-//   };
+export const getTotalDataVolunteer = async () => {
+    try {
+      const response = await axiosWithConfig.get('/volunteer-vacancies');
+      const { total_data } = response.data.pagination;
+      return total_data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return 0;
+    }
+  };
 
 
