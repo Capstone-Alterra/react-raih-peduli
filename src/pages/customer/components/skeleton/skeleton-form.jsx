e3b60b5b57d62f1b7771a1647ce18e0fca51a3b0
Sam <samilalghiffary@gmail.com>
2023-12-08 23:43:12 +0700

    refactor: Adjust skeleton layout to align with the original layout structure

--- src/pages/customer/components/skeleton/skeleton-form.jsx
+++ src/pages/customer/components/skeleton/skeleton-form.jsx
@@ -3,13 +3,29 @@ import { Skeleton } from "@/components/ui/skeleton";
 const SkeletonForm = () => {
   return (
     <>
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-4 w-40" />
-        <Skeleton className="h-10 w-full" />
+      <div className="flex gap-3">
+        <div className="flex w-full flex-col gap-2">
+          <Skeleton className="h-4 w-40" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <Skeleton className="h-4 w-40" />
+          <Skeleton className="h-10 w-full" />
+        </div>
       </div>
       <div className="flex flex-col gap-2">
         <Skeleton className="h-4 w-40" />
         <Skeleton className="h-24 w-full" />
+      </div>
+      <div className="flex gap-3">
+        <div className="flex w-full flex-col gap-2">
+          <Skeleton className="h-4 w-40" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <Skeleton className="h-4 w-40" />
+          <Skeleton className="h-10 w-full" />
+        </div>
       </div>
       <div className="flex flex-col gap-2">
         <Skeleton className="h-4 w-40" />
--- src/pages/customer/components/skeleton/skeleton-table.jsx
+++ src/pages/customer/components/skeleton/skeleton-table.jsx
@@ -9,27 +9,31 @@ const SkeletonTable = () => {
           <Skeleton className="h-5 w-5" />
         </TableCell>
         <TableCell>
+          <Skeleton className="h-3 w-32" />
+        </TableCell>
+        <TableCell>
           <div className="flex flex-col gap-2">
-            <Skeleton className="h-3 w-40" />
-            <Skeleton className="h-3 w-32" />
+            <Skeleton className="h-3 w-28" />
+            <Skeleton className="h-3 w-14" />
           </div>
         </TableCell>
         <TableCell>
           <div className="flex flex-col gap-2">
-            <Skeleton className="h-12 w-40" />
+            <Skeleton className="h-3 w-28" />
+            <Skeleton className="h-3 w-14" />
           </div>
         </TableCell>
         <TableCell>
-        <div className="flex flex-col gap-2">
-          <Skeleton className="h-3 w-96" />
-          <Skeleton className="h-3 w-96" />
-          <Skeleton className="h-3 w-96" />
-          <Skeleton className="h-3 w-96" />
-          </div>
+          <Skeleton className="h-3 w-32" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-3 w-20" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-10 w-10 rounded-full mx-auto" />
         </TableCell>
         <TableCell>
           <div className="flex gap-2">
-            <Skeleton className="h-10 w-10" />
             <Skeleton className="h-10 w-10" />
             <Skeleton className="h-10 w-10" />
           </div>
