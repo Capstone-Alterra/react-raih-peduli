58f971a641fcd0f6b0ba6eb55f8d5a9b8f750d3a
Sam <samilalghiffary@gmail.com>
2023-12-09 13:22:01 +0700

    feat: Implement skeleton loader and refactor fetching function

--- src/pages/dashboard/components/card.jsx
+++ src/pages/dashboard/components/card.jsx
@@ -1,4 +1,6 @@
-function Card({ image, title, count }) {
+import { Skeleton } from "@/components/ui/skeleton";
+
+function Card({ image, title, count, loading }) {
   return (
     <div className="bg-white p-5 w-1/4 gap-3 border-[1px] border-[#D1D1D1] rounded-lg flex">
       <div className="w-20 h-20 flex items-center justify-center rounded-lg p-2 bg-[#293066]">
@@ -6,12 +8,14 @@ function Card({ image, title, count }) {
       </div>
       <div className="flex w-min flex-col justify-between ">
         <p className="text-[#A3A4A5] text-base font-bold">{title}</p>
-        <p className="text-[#293066] text-2xl font-bold">{count}</p>
+        {loading ? (
+          <Skeleton className="h-5 w-16" />
+        ) : (
+          <p className="text-[#293066] text-2xl font-bold">{count}</p>
+        )}
       </div>
     </div>
   );
 }
 
 export default Card;
-// Example usage:
-// <Cards image={user} title="Pelanggan" customerCount={1} />
