2548c900439d1a5ded669e9fd1e1df6d4d189dde
Sam <samilalghiffary@gmail.com>
2023-12-09 13:52:22 +0700

    fix: Resolved deployment failure caused by incorrect import

--- src/pages/dashboard/components/card.jsx
+++ src/pages/dashboard/components/card.jsx
@@ -1,10 +1,10 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-function Card({ image, title, count, loading }) {
+function Card({ children, title, count, loading }) {
   return (
     <div className="bg-white p-5 w-1/4 gap-3 border-[1px] border-[#D1D1D1] rounded-lg flex">
       <div className="w-20 h-20 flex items-center justify-center rounded-lg p-2 bg-[#293066]">
-        <img className="w-9 h-9" src={image} />
+        {children}
       </div>
       <div className="flex w-min flex-col justify-between ">
         <p className="text-[#A3A4A5] text-base font-bold">{title}</p>
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
-import { getAllFundraises } from "@/utils/api/fundraise/api";
-import { getTotalDataNews } from "@/utils/api/news/api";
-import { getTotalDataCustomer } from "@/utils/api/customer/api";
-import { getVolunteer } from "@/utils/api/volunter/api";
-import Table from "./components/table-dashboard";
+import { User } from "lucide-react";
 import Header from "@/components/header";
 import Layout from "@/components/layout";
-import User from "@/assets/dashborad/User.svg";
-import News from "@/assets/dashborad/News.svg";
-import Dollar from "@/assets/dashborad/coin.svg";
-import Volunteer from "@/assets/dashborad/Hand.svg";
+import CoinIcon from "@/assets/icons/coin";
+import HandIcon from "@/assets/icons/hand";
+import NewsIcon from "@/assets/icons/news";
+import { useEffect, useState } from "react";
+import Table from "./components/table-dashboard";
 import Cards from "@/pages/dashboard/components/card";
-import { fundraiseColumn } from "./components/fundraise-column";
+import { getTotalDataNews } from "@/utils/api/news/api";
+import { getVolunteer } from "@/utils/api/volunter/api";
+import { getAllFundraises } from "@/utils/api/fundraise/api";
 import { volunteerColumn } from "./components/volunteer-column";
+import { fundraiseColumn } from "./components/fundraise-column";
+import { getTotalDataCustomer } from "@/utils/api/customer/api";
 
 function Dashboard() {
   const [loading, setLoading] = useState(false);
@@ -84,20 +84,18 @@ function Dashboard() {
     <Layout>
       <Header titleHeader="Dashboard" />
       <div className="w-full flex gap-4 mt-6">
-        <Cards image={User} title="Pelanggan" count={totalDataCustomer} loading={loading} />
-        <Cards
-          image={Dollar}
-          loading={loading}
-          title="Penggalangan Dana"
-          count={totalDataFundraise}
-        />
-        <Cards
-          loading={loading}
-          image={Volunteer}
-          title="Lowongan Sukarelawan"
-          count={totalDataVolunteer}
-        />
-        <Cards image={News} title="Berita" count={totalDataNews} loading={loading} />
+        <Cards title="Pelanggan" count={totalDataCustomer} loading={loading}>
+          <User className="text-white w-9 h-9" />
+        </Cards>
+        <Cards loading={loading} title="Penggalangan Dana" count={totalDataFundraise}>
+          <CoinIcon className="text-white w-9 h-9" />
+        </Cards>
+        <Cards loading={loading} title="Lowongan Sukarelawan" count={totalDataVolunteer}>
+          <HandIcon className="text-white w-9 h-9" />
+        </Cards>
+        <Cards title="Berita" count={totalDataNews} loading={loading}>
+          <NewsIcon className="text-white w-9 h-9" />
+        </Cards>
       </div>
       <div className="flex gap-4 mt-6">
         <Table
