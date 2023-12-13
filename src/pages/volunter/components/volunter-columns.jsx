3030bf0f26b37bfb8e9dd0ab5ce3822d522fed09
Sam <samilalghiffary@gmail.com>
2023-12-13 23:08:28 +0700

    fix: Change column order number

--- src/pages/volunter/components/volunter-columns.jsx
+++ src/pages/volunter/components/volunter-columns.jsx
@@ -1,15 +1,14 @@
+import Alert from "./alert-dialog";
 import { Link } from "react-router-dom";
 import InfoIcon from "@/assets/icons/info";
-import TrashIcon from "@/assets/icons/trash";
 import { Badge } from "@/components/ui/badge";
 import PencilIcon from "@/assets/icons/pencil";
 import { Button } from "@/components/ui/button";
-import Alert from "./alert-dialog";
 
 export const columns = [
   {
     header: "No",
-    accessorKey: "id",
+    accessorFn: (originalRow, index) => index + 1,
   },
   {
     header: "Judul Lowongan Relawan",
@@ -58,8 +57,7 @@ export const columns = [
           : "border-[#293066] bg-white hover:bg-[#293066] text-[#293066] hover:text-white";
 
       return (
-        <Badge
-          className={`font-bold flex w-24 py-2 justify-center border ${badgeClass}`}>
+        <Badge className={`font-bold flex w-24 py-2 justify-center border ${badgeClass}`}>
           {status}
         </Badge>
       );
@@ -76,7 +74,8 @@ export const columns = [
             size="icon"
             id={`btn-edit-volunter-${id}`}
             className="bg-[#E28100] hover:bg-[#E28100]/80"
-            style={{ boxShadow: "0px 4px 4px 0px #00000040" }}>
+            style={{ boxShadow: "0px 4px 4px 0px #00000040" }}
+          >
             <Link to={`/lowongan-relawan/${id}?edit=true`}>
               <PencilIcon className="w-4 h-4" />
             </Link>
@@ -86,7 +85,8 @@ export const columns = [
             size="icon"
             id={`btn-detail-volunter-${id}`}
             className="bg-[#166648] hover:bg-[#166648]/80"
-            style={{ boxShadow: "0px 4px 4px 0px #00000040" }}>
+            style={{ boxShadow: "0px 4px 4px 0px #00000040" }}
+          >
             <Link to={`/lowongan-relawan/${id}`}>
               <InfoIcon className="w-4 h-4" />
             </Link>
